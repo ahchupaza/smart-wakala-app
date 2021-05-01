a5ae3cb3d0eca5f7672b6857ccbb1563be2028d6
mibonyezo <mibonyezo@gmail.com>
2021-05-01 13:52:03 +0300

    I added and customized more activities for wakala number verification from all networks.

---
+++ app/src/main/java/com/example/smartwakala/HaloPesaNumVerificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartwakala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HaloPesaNumVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halo_pesa_num_verification);
+    }
+}
