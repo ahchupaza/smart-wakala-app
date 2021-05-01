66c3ad064d6da5be8b77a698a5783fce3d9a7624
mibonyezo <mibonyezo@gmail.com>
2021-05-02 02:32:57 +0300

    I have made major improvements in the flow and design of the App for TigoPesa services.

---
+++ app/src/main/java/com/example/smartwakala/TwoFactorAuthenticationActivity.java
@@ -0,0 +1,14 @@
+package com.example.smartwakala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoFactorAuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_factor_authentication);
+    }
+}
