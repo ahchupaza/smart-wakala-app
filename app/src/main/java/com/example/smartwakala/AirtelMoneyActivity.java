6e340b5b0e9309e258778aafa22cc7b892fe272c
roses <rose13eugen@gmail.com>
2021-05-20 13:38:46 +0300

    Add new Strings values for Airtel and TigoPesa activities.

--- app/src/main/java/com/example/smartwakala/AirtelMoneyActivity.java
+++ app/src/main/java/com/example/smartwakala/AirtelMoneyActivity.java
@@ -3,12 +3,21 @@ package com.example.smartwakala;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import static android.util.Log.d;
 
 public class AirtelMoneyActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airtel_money);
+        
     }
+
+
 }
