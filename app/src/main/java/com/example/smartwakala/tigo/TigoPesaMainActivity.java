a06435a0e4252c2a8f7711cebfe3e3a13eda78bf
mibonyezo <mibonyezo@gmail.com>
2021-07-08 07:53:38 +0300

    Added transactions for Airtelmoney, added all the required input validations as well as notifications.

--- app/src/main/java/com/example/smartwakala/tigo/TigoPesaMainActivity.java
+++ app/src/main/java/com/example/smartwakala/tigo/TigoPesaMainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.example.smartwakala.R;
 
@@ -58,8 +59,11 @@ public class TigoPesaMainActivity extends AppCompatActivity {
                 else if (akauntiYangu.isChecked()){
                     startActivity(new Intent(TigoPesaMainActivity.this, TigoPesaAkauntiYanguActivity.class));
                 }
-                else {
+                else if (toaPesaBenki.isChecked()){
                     startActivity(new Intent(TigoPesaMainActivity.this, TigoPesaWakalaKutoaBenkiActivity.class));
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Haujafanya chaguo lolote!", Toast.LENGTH_LONG).show();
                 }
             }
         });
