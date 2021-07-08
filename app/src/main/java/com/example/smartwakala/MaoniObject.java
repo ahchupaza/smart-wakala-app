7f1ccb080d66b62ed014e852405380cd486b7cb1
mibonyezo <mibonyezo@gmail.com>
2021-07-08 03:39:10 +0300

    Added maoni activity.

---
+++ app/src/main/java/com/example/smartwakala/MaoniObject.java
@@ -0,0 +1,17 @@
+package com.example.smartwakala;
+
+import android.widget.EditText;
+
+public class MaoniObject {
+
+    public String Maoni;
+
+    public MaoniObject(EditText Maoni){
+
+    }
+
+    public MaoniObject(String Maoni){
+        this.Maoni = Maoni;
+
+    }
+}
